01aa44bd8f92dad4c5b72b8780ff1281cbf643ad
Kasper Jeppe Jeppesen <kasper.jeppesen@gmail.com>
2016-05-10 22:50:42 -0700

    add almost empty servlet for handling aggregate requests

---
+++ reference/src/main/java/me/doubledutch/stroom/servlet/AggregateAPIServlet.java
@@ -0,0 +1,67 @@
+package me.doubledutch.stroom.servlet;
+
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+import java.lang.reflect.*;
+import java.io.*;
+import java.util.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import me.doubledutch.stroom.streams.*;
+
+import org.json.*;
+
+public class AggregateAPIServlet extends HttpServlet{
+	private final Logger log = Logger.getLogger("AggregateAPI");
+
+	private static StreamHandler streamHandler;
+
+	public static void setStreamHandler(StreamHandler streamHandlerArg){
+		streamHandler=streamHandlerArg;
+	}
+
+	@Override
+	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
+		try{
+			Writer out=response.getWriter();
+			response.setContentType("application/json");
+			String uriPath=request.getRequestURI().substring(request.getServletPath().length());
+			if(uriPath.startsWith("/"))uriPath=uriPath.substring(1).trim();
+			String[] splitPath=uriPath.split("/");
+			if(uriPath.length()==0){
+				// List aggregates
+				JSONArray result=new JSONArray();
+				/*
+				for(Stream stream:streamHandler.getStreams()){
+					if(stream.getTopic().indexOf(".")==-1){
+						result.put(stream.toJSON());
+					}
+				}*/
+				out.write(result.toString());
+				return;
+			}else if(splitPath.length==1){
+				// Get simple aggregate or keyset of partitioned aggregate
+				String id=splitPath[0];
+				// Stream stream=streamHandler.getOrCreateStream(topic);
+				// out.append(stream.toJSON().toString());
+			}else if(splitPath.length==2){
+				// Get partitioned aggregate by partition key
+				String id=splitPath[0];
+				String partition=splitPath[1];
+				
+			}else{
+				// response.sendError(HttpServletResponse.SC_NOT_FOUND,"You must specify both topic and location.");
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR ,"Internal server error");
+		}
+		return;
+	}
+}
