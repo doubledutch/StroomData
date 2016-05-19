2f26b9b2a913475e98fb0ba2faa21cd0027d66da
Kasper Jeppe Jeppesen <kasper.jeppesen@gmail.com>
2016-05-19 12:08:44 -0700

    raw stream view

--- reference/site/react/services.js
+++ reference/site/react/services.js
@@ -1,6 +1,15 @@
 
 var ServicePage = React.createClass({
 	render: function(){
-		return h('div','Hello World on the Services Page!')
+		var elements=[]
+		// Add header
+		elements.push(h('p','So many services...'))
+		// Add services
+		for(var i=0;i<this.props.services.length;i++){
+			var service=this.props.services[i]
+			elements.push(h('div',service.id))
+		}
+		// Return enclosing div with elements
+		return h('div',elements)
 	}
 })
--- reference/site/react/streams.js
+++ reference/site/react/streams.js
@@ -1,6 +1,15 @@
 
 var StreamPage = React.createClass({
 	render: function(){
-		return h('div','Hello World on the Streams Page!')
+		var elements=[]
+		// Add header
+		elements.push(h('p','So many streams...'))
+		// Add services
+		for(var i=0;i<this.props.streams.length;i++){
+			var stream=this.props.streams[i]
+			elements.push(h('div',stream.count+' '+stream.topic))
+		}
+		// Return enclosing div with elements
+		return h('div',elements)
 	}
 })
