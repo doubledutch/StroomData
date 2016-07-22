61184647f3aab5862f4960bd6f41fec0526fef23
Kasper Jeppe Jeppesen <kasper.jeppesen@gmail.com>
2016-07-21 21:10:26 -0700

    Further optimizations

--- reference/src/main/java/me/doubledutch/lazy/LazyParser.java
+++ reference/src/main/java/me/doubledutch/lazy/LazyParser.java
@@ -14,16 +14,6 @@ public final class LazyParser{
 		source.getChars(0,length,cbuf,0);
 	}
 
-	private final int NONE=0;
-	private final int FIELD=1;
-	private final int FIELD_INESCAPE=2;
-	private final int VALUE_SEPARATOR=3;
-	private final int VALUE=4;
-	private final int STRING=5;
-	private final int STRING_INESCAPE=6;
-
-	private int state=NONE;
-
 	// The parser uses a crude stack while parsing that maintains a reference
 	// to the top element on the stack and automatically establishes a parent
 	// child relation ship when elements are pushed onto the stack.
@@ -45,6 +35,19 @@ public final class LazyParser{
 		stackTop=stack[stackPointer-1];
 		return value;
 	}
+
+	// Pop a token off the stack and reset the stackTop pointer without returning the value
+	private void drop(){
+		stackPointer--;
+		stackTop=stack[stackPointer-1];
+	}
+
+	// Pop two tokens off the stack and reset the stackTop pointer without returning their values
+	private void doubleDrop(){
+		stackPointer-=2;
+		stackTop=stack[stackPointer-1];
+	}
+
 
 	// return the stackTop pointer
 	private final LazyToken peek(){
@@ -108,173 +111,163 @@ public final class LazyParser{
 		LazyToken token=null;
 		for(;n<length;n++){
 			c=cbuf[n];
-			switch(state){
-				case NONE:
-					switch(c){
-						case '{':
-							push(LazyToken.cObject(n));
-							break;
-						case '}':
-							// The end of an object, pop off the last value and field if any
-							token=pop();
-							if(token.type!=LazyToken.OBJECT){
-								if(token.endIndex==-1){
-									token.endIndex=n;
-								}
-								token=pop();
-								if(token.type==LazyToken.FIELD){
-									token=pop();
-								}
-								// We should now be down to the actual object
-								if(token.type!=LazyToken.OBJECT){
-									throw new LazyException("Unexpected end of object",n);
-								}
-							}
-							token.endIndex=n+1;
-							// If this object was the value for a field, pop off that field too
-							if(stackTop!=null && stackTop.type==LazyToken.FIELD){
-								pop();
-							}
-							break;
-					case '"':
-						if(stackTop.type==LazyToken.ARRAY){
-							push(LazyToken.cValue(n+1));
-							consumeString();
-							stackTop.endIndex=n;
-							// Remove value again
-							pop();
-						}else if(stackTop.type==LazyToken.FIELD){
-							push(LazyToken.cValue(n+1));
-							consumeString();
-							stackTop.endIndex=n;
-							// Remove value again
-							pop();
-							// Remove field
-							pop();
-						}else if(stackTop.type==LazyToken.OBJECT){
-							push(LazyToken.cField(n+1));
-							consumeString();
-							state=VALUE_SEPARATOR;
-							stackTop.endIndex=n;
-							tryToConsumeWhiteSpace();
-						}else{
-							// This shouldn't occur should it?
-							throw new LazyException("Syntax error",n);
+			switch(c){
+				case '{':
+					push(LazyToken.cObject(n));
+					break;
+				case '}':
+					// The end of an object, pop off the last value and field if any
+					token=pop();
+					if(token.type!=LazyToken.OBJECT){
+						if(token.endIndex==-1){
+							token.endIndex=n;
 						}
-						break;
-					case ',':
-						// This must be the end of a value and the start of another
-						if(stackTop.type==LazyToken.VALUE){
-							token=pop();
-							if(token.endIndex==-1){
-								token.endIndex=n;
-							}
-							if(stackTop.type==LazyToken.FIELD){
-								// This was the end of the value for a field, pop that too
-								pop();
-							}
-						}
-						break;
-					case '[':
-						push(LazyToken.cArray(n));
-						break;
-					case ']':
 						token=pop();
-						if(token.type!=LazyToken.ARRAY){
-							if(token.endIndex==-1){
-								token.endIndex=n;
-							}
+						if(token.type==LazyToken.FIELD){
 							token=pop();
-							if(token.type!=LazyToken.ARRAY){
-								throw new LazyException("Unexpected end of array",n);
-							}
 						}
-						token.endIndex=n+1;
-						// If this array was the value for a field, pop off that field too
-						if(stackTop!=null && stackTop.type==LazyToken.FIELD){
-							pop();
+						// We should now be down to the actual object
+						if(token.type!=LazyToken.OBJECT){
+							throw new LazyException("Unexpected end of object",n);
 						}
-						break;
-					case ' ':
-					case '\t':
-					case '\n':
-					case '\r':
-						if(stackTop!=null && stackTop.type==LazyToken.VALUE){
-							token=pop();
-							if(token.endIndex==-1){
-								token.endIndex=n;
-							}
-							if(stackTop.type==LazyToken.FIELD){
-								// This was the end of the value for a field, pop that too
-								pop();
-							}else{
-								// System.out.println("Not a field");
-							}
-						}
-						break;
-					default:
-						if(stackTop.type==LazyToken.VALUE){
-							// We are just collecting more data for the current value
-
-						}else{
-							// This must be a new value
-							if(c=='n'){
-								// Must be null value
-								if(cbuf[++n]=='u' && cbuf[++n]=='l' && cbuf[++n]=='l'){
-									push(LazyToken.cValueNull(n));
-									token=pop();
-									token.endIndex=n;
-									if(stackTop.type==LazyToken.FIELD){
-										// This was the end of the value for a field, pop that too
-										pop();
-									}
-								}else{
-									throw new LazyException("Syntax error",n);
-								}
-							}else if(c=='t'){
-								// Must be true value
-								if(cbuf[++n]=='r' && cbuf[++n]=='u' && cbuf[++n]=='e'){
-									push(LazyToken.cValueTrue(n));
-									token=pop();
-									token.endIndex=n;
-									if(stackTop.type==LazyToken.FIELD){
-										// This was the end of the value for a field, pop that too
-										pop();
-									}
-								}else{
-									throw new LazyException("Syntax error",n);
-								}
-							}else if(c=='f'){
-								// Must be false value
-								if(cbuf[++n]=='a' && cbuf[++n]=='l' && cbuf[++n]=='s' && cbuf[++n]=='e'){
-									push(LazyToken.cValueFalse(n));
-									token=pop();
-									token.endIndex=n;
-									if(stackTop.type==LazyToken.FIELD){
-										// This was the end of the value for a field, pop that too
-										pop();
-									}
-								}else{
-									throw new LazyException("Syntax error",n);
-								}
-							}else if(c=='-' || !(c<'0' || c>'9')){
-								// Must be a number
-								push(LazyToken.cValue(n));
-								// TODO: we should really validate the syntax of a number
-							}
-							
-						}
-						break;
+					}
+					token.endIndex=n+1;
+					// If this object was the value for a field, pop off that field too
+					if(stackTop!=null && stackTop.type==LazyToken.FIELD){
+						drop();
 					}
 					break;
-				case VALUE_SEPARATOR:
+			case '"':
+				if(stackTop.type==LazyToken.ARRAY){
+					push(LazyToken.cValue(n+1));
+					consumeString();
+					stackTop.endIndex=n;
+					// Remove value again
+					drop();
+				}else if(stackTop.type==LazyToken.FIELD){
+					push(LazyToken.cValue(n+1));
+					consumeString();
+					stackTop.endIndex=n;
+					// Remove value and field again
+					doubleDrop();
+				}else if(stackTop.type==LazyToken.OBJECT){
+					push(LazyToken.cField(n+1));
+					consumeString();
+					stackTop.endIndex=n;
+					tryToConsumeWhiteSpace();
+					c=cbuf[++n];
 					if(c==':'){
-						state=NONE;
 						tryToConsumeWhiteSpace();
 					}else{
 						throw new LazyException("Unexpected character! Was expecting field separator ':'",n);
 					}
-					break;
+				}else{
+					// This shouldn't occur should it?
+					throw new LazyException("Syntax error",n);
+				}
+				break;
+			case ',':
+				// This must be the end of a value and the start of another
+				if(stackTop.type==LazyToken.VALUE){
+					token=pop();
+					if(token.endIndex==-1){
+						token.endIndex=n;
+					}
+					if(stackTop.type==LazyToken.FIELD){
+						// This was the end of the value for a field, pop that too
+						drop();
+					}
+				}
+				break;
+			case '[':
+				push(LazyToken.cArray(n));
+				break;
+			case ']':
+				token=pop();
+				if(token.type!=LazyToken.ARRAY){
+					if(token.endIndex==-1){
+						token.endIndex=n;
+					}
+					token=pop();
+					if(token.type!=LazyToken.ARRAY){
+						throw new LazyException("Unexpected end of array",n);
+					}
+				}
+				token.endIndex=n+1;
+				// If this array was the value for a field, pop off that field too
+				if(stackTop!=null && stackTop.type==LazyToken.FIELD){
+					drop();
+				}
+				break;
+			case ' ':
+			case '\t':
+			case '\n':
+			case '\r':
+				if(stackTop!=null && stackTop.type==LazyToken.VALUE){
+					token=pop();
+					if(token.endIndex==-1){
+						token.endIndex=n;
+					}
+					if(stackTop.type==LazyToken.FIELD){
+						// This was the end of the value for a field, pop that too
+						drop();
+					}else{
+						// System.out.println("Not a field");
+					}
+				}
+				break;
+			default:
+				if(stackTop.type==LazyToken.VALUE){
+					// We are just collecting more data for the current value
+				}else{
+					// This must be a new value
+					if(c=='n'){
+						// Must be null value
+						if(cbuf[++n]=='u' && cbuf[++n]=='l' && cbuf[++n]=='l'){
+							push(LazyToken.cValueNull(n));
+							token=pop();
+							token.endIndex=n;
+							if(stackTop.type==LazyToken.FIELD){
+								// This was the end of the value for a field, pop that too
+								drop();
+							}
+						}else{
+							throw new LazyException("Syntax error",n);
+						}
+					}else if(c=='t'){
+						// Must be true value
+						if(cbuf[++n]=='r' && cbuf[++n]=='u' && cbuf[++n]=='e'){
+							push(LazyToken.cValueTrue(n));
+							token=pop();
+							token.endIndex=n;
+							if(stackTop.type==LazyToken.FIELD){
+								// This was the end of the value for a field, pop that too
+								drop();
+							}
+						}else{
+							throw new LazyException("Syntax error",n);
+						}
+					}else if(c=='f'){
+						// Must be false value
+						if(cbuf[++n]=='a' && cbuf[++n]=='l' && cbuf[++n]=='s' && cbuf[++n]=='e'){
+							push(LazyToken.cValueFalse(n));
+							token=pop();
+							token.endIndex=n;
+							if(stackTop.type==LazyToken.FIELD){
+								// This was the end of the value for a field, pop that too
+								drop();
+							}
+						}else{
+							throw new LazyException("Syntax error",n);
+						}
+					}else if(c=='-' || !(c<'0' || c>'9')){
+						// Must be a number
+						push(LazyToken.cValue(n));
+						// TODO: we should really validate the syntax of a number
+					}
+					
+				}
+				break;
 			}
 		}
 	}
